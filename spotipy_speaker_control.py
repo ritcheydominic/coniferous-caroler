fb73d9c2458fd6647c826451b55e11f9054651a8
AndrewCouncil <andrew.p.council@gmail.com>
2023-11-30 20:52:35 -0500

    test the full stack

--- spotipy_speaker_control.py
+++ spotipy_speaker_control.py
@@ -1,10 +1,15 @@
 import random
+from time import sleep
+import bluetooth
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from spotipy import util
 cid = '500d52c25b89477dae46b95e7391bab5'
 secret = 'ec17f112c45a42b690b17a6a5c8a2aca'
 
+device_to_playlist = {
+    "B0:4A:6A:98:EB:10": "https://open.spotify.com/playlist/72dZDJTedH4uwVuy7Dlpva?si=ab186ebe6f8c45d9",
+    "testuser2": "https://open.spotify.com/playlist/5bK5b3kFIGRoX3hNCkmKSd?si=e6535dfe594649db",
+}
 # scope to see avaiable devices and manage playback
 scope = 'playlist-modify-public playlist-modify-private playlist-read-private user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing'
 
@@ -19,44 +24,71 @@ for device in devices['devices']:
         print(f"Found device {device_name} with id {device_id}")
         break
 
-device_to_playlist = {
-    "B0:4A:6A:98:EB:10": "https://open.spotify.com/playlist/72dZDJTedH4uwVuy7Dlpva?si=ab186ebe6f8c45d9",
-    "testuser2": "https://open.spotify.com/playlist/5bK5b3kFIGRoX3hNCkmKSd?si=e6535dfe594649db",
-}
+
+def get_connected_devices():
+    return bluetooth.discover_devices(lookup_names=False, lookup_class=False, device_id=-1, duration=8)
+
+def update_playback(devices):
+    track_uris = []
+
+    for dev in devices:
+        user_playlist_link = device_to_playlist[dev]
+        user_playlist_id = user_playlist_link.split('/')[-1].split('?')[0]
+        results = sp.playlist_tracks(user_playlist_id)
+        track_uris.extend([track['track']['uri'] for track in results['items']])
+
+    # randomize the list
+    random.shuffle(track_uris)
+
+    # configure queue playlist
+    queue_playlist_uri = 'spotify:playlist:0so5DFBpArVy7FbECcr7g2'
+    queue_playlist_id = queue_playlist_uri.split(':')[-1]
+
+    # clear queue playlist
+    results = sp.playlist_tracks(queue_playlist_id)
+    queue_removal_uris = [track['track']['uri'] for track in results['items']]
+    sp.playlist_remove_all_occurrences_of_items(queue_playlist_id, queue_removal_uris)
+
+    # add in new tracks
+    sp.playlist_add_items(queue_playlist_id, track_uris)
+
+    # connect to device
+    sp.transfer_playback(device_id=device_id)
+
+    # turn off shuffle
+    sp.shuffle(False, device_id=device_id)
+
+    # start playback
+    sp.start_playback(device_id=device_id, context_uri=queue_playlist_uri)
+
+def monitor_devices():
+    registered_connected_devices = []
+    while True:
+        connected_devices = get_connected_devices()
+        old_registered_devices_count = len(registered_connected_devices)
+        registered_connected_devices = [dev for dev in connected_devices if dev in device_to_playlist.keys()]
+
+        print("Connected Bluetooth Devices:")
+        if registered_connected_devices:
+            for device in registered_connected_devices:
+                print(f"Address: {device}, Playlist: {device_to_playlist[device]}")
+                print("\n")
+        else:
+            print("No registered Bluetooth devices found.")
+
+        # Number of connected devices has changed
+        if len(registered_connected_devices) != old_registered_devices_count:
+            print("Connected devices changed")
+            if registered_connected_devices:
+                # new set of devices are connected
+                update_playback(registered_connected_devices)
+
+            else:
+                # no devices connected, stop playback
+                sp.pause_playback(device_id=device_id)
+        
+        sleep(30)
 
 
-
-# # add a public playlist to the queue
-user_playlist_link = 'https://open.spotify.com/playlist/72dZDJTedH4uwVuy7Dlpva?si=ab186ebe6f8c45d9'
-user_playlist_id = user_playlist_link.split('/')[-1].split('?')[0]
-
-# get list of track uris
-track_uris = []
-results = sp.playlist_tracks(user_playlist_id)
-tracks = results['items']
-track_uris = [track['track']['uri'] for track in results['items']]
-
-# randomize the list
-random.shuffle(track_uris)
-
-# configure queue playlist
-queue_playlist_uri = 'spotify:playlist:0so5DFBpArVy7FbECcr7g2'
-queue_playlist_id = queue_playlist_uri.split(':')[-1]
-
-# clear queue playlist
-results = sp.playlist_tracks(queue_playlist_id)
-queue_removal_uris = [track['track']['uri'] for track in results['items']]
-sp.playlist_remove_all_occurrences_of_items(queue_playlist_id, queue_removal_uris)
-
-# add in new tracks
-sp.playlist_add_items(queue_playlist_id, track_uris)
-
-# connect to device
-sp.transfer_playback(device_id=device_id)
-
-# turn off shuffle
-sp.shuffle(False, device_id=device_id)
-
-# start playback
-sp.start_playback(device_id=device_id, context_uri=queue_playlist_uri)
-
+if __name__ == "__main__":
+    monitor_devices()
